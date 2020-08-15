f3b9296e8c46a9daff51974b2f42b7255a6918d3
koji-torishima <torikou3685@gmail.com>
2020-08-16 06:51:09 +0900

    関心の分離２　＋ーボタンの分離

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import CounterText from './CounterText'; // カウントするTextを分離し、ここにimport
+import PlusButton from './PlusButton';
+import ReduceButton from './ReduceButton';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,48 +11,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
   },
-
-  button: {
-    backgroundColor: '#008080',
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-  },
-
-  disabledButton: {
-    backgroundColor: 'gray',
-  },
 });
 
 export default function Counter() {
   // ここで初期値を入れないとアンデファインドになる
   const [count, setCounter] = React.useState(0);
 
-  const addCounter = React.useCallback(() => {
-    setCounter(count + 1);
-  }, [count]);
-
   const reduceCounter = React.useCallback(() => {
     setCounter(count - 1);
   }, [count]);
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={addCounter}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <PlusButton count={count} setCounter={setCounter} />
       <CounterText count={count} />
-      <TouchableOpacity
-        style={[styles.button, count <= 0 && styles.disabledButton]}
-        onPress={reduceCounter}
-        disabled={count <= 0}>
-        <Text style={styles.buttonText}>-</Text>
-      </TouchableOpacity>
+      <ReduceButton count={count} setCounter={setCounter} />
     </View>
   );
 }
