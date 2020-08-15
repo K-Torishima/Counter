c2695391a763623fd1424399bbeb422dd977582a
koji-torishima <torikou3685@gmail.com>
2020-08-15 20:52:05 +0900

    コメントの追加

--- src/index.tsx
+++ src/index.tsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
 });
 
 export default function Counter() {
+  // ここで初期値を入れないとアンデファインドになる
   const [count, setCounter] = React.useState(0);
 
   const addCounter = React.useCallback(() => {
